86f298cc29c864cda729e77aa1b5f9a4a58a31d5
Matt Pope <UnknownGuardian@gmail.com>
2020-03-23 14:43:05 -0600

    Sorry

--- functions/auth/auth.ts
+++ functions/auth/auth.ts
@@ -1,33 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const secret = process.env.TOKEN_SECRET;
 
-
 const auth = () => {
-    return ({
-        before: (handler,next) => {
-            const token = handler.event.queryStringParameters.token;
-            jwt.verify(token, secret, (error, decoded) => {
-                if(error){
-                    console.log('error x', error);
-                    throw new Error(error.message);
-                } else {
-                    decoded;
-                    // console.log('decoded', decoded);
-                    // handler.event.authenticated = decoded;
-                    return next();
-                }
-            })
-        },
-        onError: (handler) => {
-            // console.log('throwing error: ', handler.error);
-            return handler.callback(null, {
-                statusCode: 401,
-                body: JSON.stringify({error: handler.error.message})
-            })
+  return {
+    before: handler => {
+      const token = handler.event.queryStringParameters.token;
+      jwt.verify(token, secret, (error, decoded) => {
+        if (error) {
+          console.log("error x", error);
+          throw new Error(error.message);
+        } else {
+          decoded;
+          // console.log('decoded', decoded);
+          // handler.event.authenticated = decoded;
+          return decoded;
         }
-    })
-}
+      });
+    },
+    onError: handler => {
+      return Promise.resolve({
+        statusCode: 401,
+        body: JSON.stringify({ error: handler.error.message })
+      });
+    }
+  };
+};
 
-export {
-    auth
-}
+export { auth };
